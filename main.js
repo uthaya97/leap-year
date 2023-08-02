2b02be0e2e28ba1ffb42564acc4e11f6dae05268
paulkani <paulkani.s@golden-hippo.com>
2023-08-02 20:00:29 +0530

    leaf

---
+++ main.js
@@ -0,0 +1,42 @@
+// var today=new Date();
+// console.log(today);
+// var year=today.getFullYear();
+// console.log(year);
+// if(year%4==0){
+//     console.log(year+" "+"This year is leaf year");
+// }
+// else{
+//     console.log(year+" "+"This year is not leaf year");
+// }
+    
+
+
+
+    var today=new Date();
+    console.log(today);
+    today.setFullYear(2025);
+    var b=today.getFullYear();
+    console.log(b);
+    if(b%4==0){
+        document.write(b+"<br>"+"<br>"+"This year is leap year");
+    }
+    else{
+        document.write(b+"<br>"+"<br>"+"This year is not leap year");
+    }
+
+
+    // var today=new Date();
+    // console.log(today);
+    // var b=today.getDay();
+    // b.toString();
+    // console.log(b);
+    // if((b==0)||(b==6)){
+    //     document.write(b+" "+"This is week end");
+    // }
+    // else{
+    //     document.write(b+" "+ "This is not week end");
+    // }
+
+      
+    
+    
